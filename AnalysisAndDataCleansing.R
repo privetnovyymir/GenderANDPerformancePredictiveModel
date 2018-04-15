a5626bd6d1aab8abd55e032c3da5b32ebe36f81d
privetnovyymir <i.ojeda.a21@gmail.com>
2018-04-15 22:46:37 +0200

    Plotting Data and new library ==> 'rmarkdown'

--- AnalysisAndDataCleansing.R
+++ AnalysisAndDataCleansing.R
@@ -8,10 +8,18 @@ library(janitor)
 library(data.table)
 library(knitr)
 library(tidyverse)
+library(rmarkdown)
 
 summary(train)
 str(train) # 18,255 rows * 1,235 cols
 head(train[,c(1:20)])# We must predict is_female variable, located at 10th column
 sum_is_female <- sum(train$is_female)
-num_females <- sum_is_female / 18255
-num_females # ~ 54% o/ sample
+    num_females <- sum_is_female / 18255
+    num_females # ~ 54% o/ sample
+    train %>% ggplot(aes(as.factor(is_female),fill=as.factor(is_female)),guide=FALSE) + 
+    geom_histogram(stat="count") + 
+    scale_y_continuous(name="Count",labels = scales::comma,limits = c(0,10000)) +
+    xlab("is_female (var)") +
+    ggtitle(" Female(1) vs Male(1)")
+
+    
