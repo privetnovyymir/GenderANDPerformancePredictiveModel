ffe163640eb3b93d19289e3334272f24585bfaf0
privetnovyymir <i.ojeda.a21@gmail.com>
2018-04-16 03:10:05 +0200

    Init and Machine Learning libraries loaded

---
+++ PredictionsAndBehaviourWMachineLearning.R
@@ -0,0 +1,15 @@
+# Applied libraries for Modeling and Training
+library(data.table)
+library(stringr)
+library(readxl)
+library(e1071)
+library(rpart)
+library(caret)
+library(randomForest) # apart from e1071 content ::randoforest
+library(xgboost)
+library(mboost)
+library(pROC)
+library(knitr)
+library(tidyverse) # conflicts unmasked when library loaded
+
+
